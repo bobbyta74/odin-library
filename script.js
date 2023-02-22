4b2c594910c2c5d3405087ca5b38b1231ae4321d
bobbyta74 <bobbyta74@gmail.com>
2023-02-22 19:42:10 +0100

    Style, enable adding new books using form

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 let myLibrary = [];
 const bookshelf = document.querySelector("#bookshelf");
+const addbook = document.querySelector("#addbook");
+const myform = document.querySelector("form");
+const submit = document.querySelector("#submit");
+const body = document.querySelector("body");
 
-function Book(title, pages, read) {
+const inp_title = document.querySelector("#title");
+const inp_author = document.querySelector("#author");
+const inp_pages = document.querySelector("#pages");
+const inp_read = document.querySelector("#read");
+
+function Book(title, author, pages, read) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
     if (read == true) {
         this.read = "read";
@@ -12,17 +22,14 @@ function Book(title, pages, read) {
 }
 
 Book.prototype.info = function () {
-    return `${this.title}, ${this.pages} pages, ${this.read}`
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
 }
 
-const mobydick = new Book("Moby Dick", "400", false);
+const mobydick = new Book("Moby Dick", "Herman Melville", 400, false);
 myLibrary.push(mobydick);
 
-function addBookToLibrary () {
-
-}
-
 function displayBooks () {
+    bookshelf.textContent = "";
     for (let book of myLibrary) {
         const mydiv = document.createElement("div");
         mydiv.textContent = book.info()
@@ -31,3 +38,23 @@ function displayBooks () {
 }
 
 displayBooks();
+
+addbook.addEventListener("click", function () {
+    myform.style.filter = "none";
+    myform.style.display = "flex";
+})
+
+function addBookToLibrary () {
+    let newbook = new Book(inp_title.value, inp_author.value, inp_pages.value, inp_read.checked);
+    myLibrary.push(newbook);
+    inp_title.value = "";
+    inp_author.value = "";
+    inp_pages.value = 0;
+    read.checked = false;
+    myform.style.display = "none";
+}
+
+submit.addEventListener("click", function () {
+    addBookToLibrary();
+    displayBooks();
+});
